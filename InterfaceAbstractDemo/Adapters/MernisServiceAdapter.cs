e09e96a632cffb2080f21aae4e41bb7f0114bfb6
salihdurus <durussalih@gmail.com>
2021-09-20 03:45:01 +0300

    5.Gün Ödev 4

---
+++ InterfaceAbstractDemo/Adapters/MernisServiceAdapter.cs
@@ -0,0 +1,22 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Text;
+//https://tckimlik.nvi.gov.tr/Service/KPSPublic.asmx?WSDL
+namespace InterfaceAbstractDemo.Adapters
+{
+    class MernisServiceAdapter : ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            return client.TCKimlikNoDogrulaAsync(
+                new TCKimlikNoDogrulaRequest(
+                    new TCKimlikNoDogrulaRequestBody(
+                        Convert.ToInt64(customer.NationalityId), customer.FirstName.ToUpper(), customer.LastName.ToUpper(), customer.DateOfBirth.Year))).Result.Body.TCKimlikNoDogrulaResult;
+
+        }
+    }
+}
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer { FirstName = "Salih", LastName = "Dürüs", DateOfBirth = new DateTime(1999, 10, 24), NationalityId = "100000001" });
+
+        }
+    }
+}
